3555de48691698553ee63561074aa45adf7ff3d6
evgeniy selevko <evgeniyselevko@gmail.com>
2016-10-21 14:15:59 +0300

    add material path

--- MaterialDesingAsset.php
+++ MaterialDesingAsset.php
@@ -10,5 +10,11 @@ use yii\web\AssetBundle;
 class MaterialDesingAsset extends AssetBundle
 {
 	
-
+public 	$sourcePath = '@bower/materialize';
+public $css = [
+		'dist/css/material.min.css'	
+	];
+	public $js = [	
+		'dist/js/material.min.js'	
+	];	
 }
